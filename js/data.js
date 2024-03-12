6381f111484aa95e994893030fcdd5eacdad08ee
Ramiro Andres Garcia Urueta <ramirogarciaurueta1005@gmail.com>
2024-03-11 21:17:19 -0500

    se agrego sandman

---
+++ js/data.js
@@ -0,0 +1,9 @@
+export const data = [
+    {
+        name: "Sandman",
+        image: "https://i.blogs.es/6d005b/sandman-cartel/450_1000.jpeg",
+        description: "Cuando Sandman, el ser cósmico que controla los sueños, es capturado y mantenido prisionero durante más de un siglo, debe viajar a través de diferentes mundos y líneas de tiempo para arreglar el caos que ha causado su ausencia.",
+        categories: "",
+        actors: [""]
+    }
+]
