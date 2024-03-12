1b79532ec78c493053cebc15aa6b23bf3884a8d3
Ramiro Andres Garcia Urueta <ramirogarciaurueta1005@gmail.com>
2024-03-11 21:22:11 -0500

    se creo la funcion que creara las cards

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 (() => {
     const cardContainer = document.getElementById("card-movie-container");
+
+    const createCardItem = ({
+        name,
+        image,
+        description
+    }) => {
+        return `<div class="card" style="width: 18rem;">
+        <img src="${image}" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+          <a href="#" class="btn btn-primary">View</a>
+        </div>
+      </div>`
+    }
 })()
