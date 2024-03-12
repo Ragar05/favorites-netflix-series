094da10ab49bef331a6470aeaa55f0744d407b9c
Ramiro Andres Garcia Urueta <ramirogarciaurueta1005@gmail.com>
2024-03-11 21:17:36 -0500

    se agrego black mirror

--- js/data.js
+++ js/data.js
@@ -5,5 +5,12 @@ export const data = [
         description: "Cuando Sandman, el ser cósmico que controla los sueños, es capturado y mantenido prisionero durante más de un siglo, debe viajar a través de diferentes mundos y líneas de tiempo para arreglar el caos que ha causado su ausencia.",
         categories: "",
         actors: [""]
+    },
+    {
+        name: "Black mirror",
+        image: "https://pbs.twimg.com/media/D7WH3W-W4AYeMy0.jpg",
+        description: "El lado oscuro de la era tecnológica en la que se vive: la paranoia de ser vigilados como en un panóptico, los usos terroristas de las nuevas herramientas y su relación con la experiencia cotidiana.",
+        categories: "",
+        actors: [""]
     }
 ]
