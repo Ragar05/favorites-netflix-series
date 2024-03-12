4070a50a7baf2ffb80c1ba3947f9c318019fcbbe
Ramiro Andres Garcia Urueta <ramirogarciaurueta1005@gmail.com>
2024-03-11 21:18:10 -0500

    se agrego la casa de papel

--- js/data.js
+++ js/data.js
@@ -19,5 +19,12 @@ export const data = [
         description: "El gran abogado corporativo de Manhattan, Harvey Specter y su equipo, Donna Paulsen, Louis Litt y Alex Williams, se lanzan a un juego por obtener el poder cuando un nuevo socio se une a la empresa. Con sus dos mejores colegas fuera y Jessica de regreso en Chicago, Specter y el equipo intentan adaptarse a una nueva situación sin ellos. El grupo enfrenta traiciones, relaciones fieras y secretos que finalmente salen a la luz. Viejas y nuevas rivalidades aparecen entre los miembros del equipo a medida que aprenden a lidiar con el recién llegado.",
         categories: "",
         actors: [""]
+    },
+    {
+        name: "La casa de papel",
+        image: "https://www.aceprensa.com/wp-content/uploads/2019/08/71446-1-683x1024.jpg",
+        description: "Una banda organizada de ladrones se propone cometer el atraco del siglo en la Fábrica Nacional de Moneda y Timbre. Cinco meses de preparación quedarán reducidos a once días para poder llevar a cabo con éxito el gran golpe.",
+        categories: "",
+        actors: [""]
     }
 ]
